8ad1b84280199326bb9520b0f0838b31c1c4e43c
Sasaki Akira <akira@Dev.local>
2016-02-11 17:12:14 +0900

    Update lib.

--- src/fabo-mpu9250.cpp
+++ src/fabo-mpu9250.cpp
@@ -1,101 +1,104 @@
-#include "fabo-adxl345.h"
+#include "fabo-mpu9250.h"
 
-bool adxl345::searchDevice()
+bool FaBo9Axis::searchDevice()
 {
-    
-  byte device = 0x00;
-  readI2c(ADXL345_DEVID_REG, 1, &device);
+  byte device_mpu9250 = 0x00;
+  readI2c(MPU9250_SLAVE_ADDRESS, MPU9250_WHO_AM_I_REG, 1, &device_mpu9250);
+
+  byte device_ak8963 = 0x00;
+  readI2c(AK8963_SLAVE_ADDRESS, AK8963_DEVICE_ID_REG, 1, &device_ak8963);
   
-  if(device == ADXL345_DEVICE){
-      return true;
+  if(device_mpu9250 == MPU9250_DEVICE_NAME && device_ak8963 == AK8963_DEVICE_NAME){
+    return true;
   } else{
-      return false;
+    return false;
   }
 }
 
-void adxl345::powerOn()
+void FaBo9Axis::powerOn()
 {
-   byte POWER = ADXL345_AUTO_SLEEP_OFF;
-   POWER |= ADXL345_MEASURE_ON;
-   POWER |= ADXL345_SLEEP_OFF;
-   POWER |= ADXL345_WAKEUP_8HZ;
-   
-   writeI2c(ADXL345_POWER_CTL_REG, POWER);
+  writeI2c(MPU9250_SLAVE_ADDRESS, MPU9250_POWER_MNG1_REG, 0x00);
+  writeI2c(MPU9250_SLAVE_ADDRESS, MPU9250_POWER_MNG2_REG, 0x00);
 }
 
-void adxl345::readXYZ(int *x, int *y, int *z)
+void FaBo9Axis::reset()
+{
+  writeI2c(MPU9250_SLAVE_ADDRESS, MPU9250_POWER_MNG1_REG, 0x80);
+}
+
+void FaBo9Axis::configuration()
+{
+  byte accel_conf = MPU9250_ACCELL_OUTPUT_BAND_1_13K;
+  writeI2c(MPU9250_SLAVE_ADDRESS, MPU9250_ACCEL_CONFIG2_REG, accel_conf);
+
+  byte gyro_conf = MPU9250_FCHOICE_B_BAND_0;
+  writeI2c(MPU9250_SLAVE_ADDRESS, MPU9250_GYRO_CONFIG_REG, gyro_conf);
+
+  byte conf = MPU9250_EXT_SYNC_SET_DISABLE;
+  conf |= MPU9250_DLPF_CFG_0;
+  writeI2c(MPU9250_SLAVE_ADDRESS, MPU9250_CONFIGURATION_REG, conf);
+
+  byte ak8963_ctrl = AK8963_BIT_16_OUTPUT;
+  ak8963_ctrl |= AK8963_MODE_SINGLE;
+  writeI2c(AK8963_SLAVE_ADDRESS, AK8963_CTRL1_REG, ak8963_ctrl);
+}
+
+void FaBo9Axis::readAccelXYZ(int *x, int *y, int *z)
 {
   uint8_t length = 6;
-  byte axis_buff[6];
-  readI2c(0x32, length, axis_buff); //レジスターアドレス 0x32から6バイト読む
-  *x = (((int)axis_buff[1]) << 8) | axis_buff[0];   
-  *y = (((int)axis_buff[3]) << 8) | axis_buff[2];
-  *z = (((int)axis_buff[5]) << 8) | axis_buff[4];
+  byte accel_buff[6];
+  readI2c(MPU9250_SLAVE_ADDRESS, MPU9250_ACCEL_X_MSB_REG, length, accel_buff); 
+  *x = (((int)accel_buff[0]) << 8) | accel_buff[1];   
+  *y = (((int)accel_buff[2]) << 8) | accel_buff[3];
+  *z = (((int)accel_buff[4]) << 8) | accel_buff[5];
 }
 
-byte adxl345::readIntStatus(){
-
-    byte buff;
-    readI2c(ADXL345_INT_SOURCE_REG, 1, &buff); //レジスターアドレス 0x32から6バイト読む
-    
-    return buff;
-}
-                           
-void adxl345::enableTap()
-{  
-  writeI2c(ADXL345_THRESH_TAP_REG, 50); // 単位 62.5mg/LBS
-  writeI2c(ADXL345_DUR_REG, 15); // 単位 1.25ms/LSB
-  writeI2c(ADXL345_LATENT_REG, 120); // 単位 1.25ms/LSB
-  writeI2c(ADXL345_WINDOW_REG, 200); // 単位 1.25ms/LSB
-}
-
-bool adxl345::isSingleTap(byte value)
+void FaBo9Axis::readGyroXYZ(int *x, int *y, int *z)
 {
-  if((value & 0b01000000) == 0b01000000){
-    return true;
-  } else {
-    return false;
-  }
+  uint8_t length = 6;
+  byte gyro_buff[6];
+  readI2c(MPU9250_SLAVE_ADDRESS, MPU9250_GYRO_X_MSB_REG, length, gyro_buff); 
+  *x = (((int)gyro_buff[0]) << 8) | gyro_buff[1];   
+  *y = (((int)gyro_buff[2]) << 8) | gyro_buff[3];
+  *z = (((int)gyro_buff[4]) << 8) | gyro_buff[5];
 }
 
-bool adxl345::isDoubleTap(byte value)
+uint16_t FaBo9Axis::readTemperature()
 {
-  if((value & 0b00100000) == 0b00100000){
-    return true;
-  } else {
-    return false;
-  }
+  uint8_t length = 2;
+  byte temp_buff[2];
+  readI2c(MPU9250_SLAVE_ADDRESS, MPU9250_TEMP_MSB_REG, length, temp_buff); 
+  return (((int)temp_buff[0]) << 8) | temp_buff[1];   
 }
 
-void adxl345::configuration()
+void FaBo9Axis::readMagnetXYZ(int *x, int *y, int *z)
 {
-   byte CONF = ADXL345_SELF_TEST_OFF;
-   CONF |= ADXL345_SPI_OFF;
-   CONF |= ADXL345_INT_INVERT_OFF;
-   CONF |= ADXL345_FULL_RES_OFF;
-   CONF |= ADXL345_JUSTIFY_OFF;
-   CONF |= ADXL345_RANGE_16G;
-   writeI2c(ADXL345_DATA_FORMAT_REG, CONF);
+  uint8_t length = 6;
+  byte maget_buff[6];
+  readI2c(AK8963_SLAVE_ADDRESS, AK8963_MANGET_X_LSB_REG, length, maget_buff); 
+  *x = (((int)maget_buff[1]) << 8) | maget_buff[0];   
+  *y = (((int)maget_buff[3]) << 8) | maget_buff[2];
+  *z = (((int)maget_buff[5]) << 8) | maget_buff[4];
 }
 
 // I2Cへの書き込み
-void adxl345::writeI2c(byte register_addr, byte value) {
+void FaBo9Axis::writeI2c(uint8_t address, byte register_addr, byte value) {
   Wire.begin();       // I2Cの開始
-  Wire.beginTransmission(ADXL345_SLAVE_ADDRESS);  
+  Wire.beginTransmission(address);  
   Wire.write(register_addr);         
   Wire.write(value);                 
   Wire.endTransmission();        
 }
 
 // I2Cへの読み込み
-void adxl345::readI2c(byte register_addr, int num, byte buffer[]) {
+void FaBo9Axis::readI2c(uint8_t address, byte register_addr, int num, byte buffer[]) {
   Wire.begin();       // I2Cの開始
-  Wire.beginTransmission(ADXL345_SLAVE_ADDRESS); 
+  Wire.beginTransmission(address); 
   Wire.write(register_addr);           
   Wire.endTransmission();         
 
-  Wire.beginTransmission(ADXL345_SLAVE_ADDRESS); 
-  Wire.requestFrom(ADXL345_SLAVE_ADDRESS, num);   
+  Wire.beginTransmission(address); 
+  Wire.requestFrom(address, num);   
 
   int i = 0;
   while(Wire.available())        
@@ -105,5 +108,3 @@ void adxl345::readI2c(byte register_addr, int num, byte buffer[]) {
   }
   Wire.endTransmission();         
 }
-
-adxl345 fabo3Axis;
--- src/fabo-mpu9250.h
+++ src/fabo-mpu9250.h
@@ -1,63 +1,103 @@
 #include "Arduino.h"
 #include "Wire.h"
 
-#define ADXL345_SLAVE_ADDRESS (0x53)
+// Slave address
+#define MPU9250_SLAVE_ADDRESS 0x68
+#define AK8963_SLAVE_ADDRESS 0x0C
 
-#define ADXL345_DEVID_REG 0x00
-#define ADXL345_THRESH_TAP_REG 0x1D
-#define ADXL345_DUR_REG 0x22
-#define ADXL345_LATENT_REG 0x23
-#define ADXL345_WINDOW_REG 0x24
-#define ADXL345_POWER_CTL_REG 0x2D
-#define ADXL345_TAP_STATUS_REG 0x2B
-#define ADXL345_TAP_AXES_REG 0x2A
-#define ADXL345_INT_SOURCE_REG 0x30
-#define ADXL345_DATA_FORMAT_REG 0x31
+// Who am I (device id)
+#define MPU9250_WHO_AM_I_REG 0x75
+#define AK8963_DEVICE_ID_REG 0x00
 
-#define ADXL345_SELF_TEST_ON 0b10000000
-#define ADXL345_SELF_TEST_OFF 0b00000000
-#define ADXL345_SPI_ON 0b01000000
-#define ADXL345_SPI_OFF 0b00000000
-#define ADXL345_INT_INVERT_ON 0b00100000
-#define ADXL345_INT_INVERT_OFF 0b00000000
-#define ADXL345_FULL_RES_ON 0b00001000
-#define ADXL345_FULL_RES_OFF 0b00000000
-#define ADXL345_JUSTIFY_ON 0b00000100
-#define ADXL345_JUSTIFY_OFF 0b00000000
+// Power management
+#define MPU9250_POWER_MNG1_REG 0x6B
+#define MPU9250_POWER_MNG2_REG 0x6C
 
-#define ADXL345_RANGE_2G 0b00
-#define ADXL345_RANGE_4G 0b01
-#define ADXL345_RANGE_8G 0b10
-#define ADXL345_RANGE_16G 0b11
+// Read accel
+#define MPU9250_ACCEL_X_MSB_REG 0x3B
+#define MPU9250_ACCEL_X_LSB_REG 0x3C
+#define MPU9250_ACCEL_Y_MSB_REG 0x3D
+#define MPU9250_ACCEL_Y_LSB_REG 0x3E
+#define MPU9250_ACCEL_Z_MSB_REG 0x3F
+#define MPU9250_ACCEL_Z_LSB_REG 0x40
 
-#define ADXL345_AUTO_SLEEP_ON 0b00010000
-#define ADXL345_AUTO_SLEEP_OFF 0b00000000
-#define ADXL345_MEASURE_ON 0b00001000
-#define ADXL345_MEASURE_OFF 0b00000000
-#define ADXL345_SLEEP_ON 0b00000100
-#define ADXL345_SLEEP_OFF 0b00000000
+// Read Temperature
+#define MPU9250_TEMP_MSB_REG 0x41
+#define MPU9250_TEMP_LSB_REG 0x42
 
-#define ADXL345_WAKEUP_8HZ 0b00
-#define ADXL345_WAKEUP_4HZ 0b01
-#define ADXL345_WAKEUP_2HZ 0b10
-#define ADXL345_WAKEUP_1HZ 0b11
+// Read Gyroscope
+#define MPU9250_GYRO_X_MSB_REG 0x43
+#define MPU9250_GYRO_X_LSB_REG 0x44
+#define MPU9250_GYRO_Y_MSB_REG 0x45
+#define MPU9250_GYRO_Y_LSB_REG 0x46
+#define MPU9250_GYRO_Z_MSB_REG 0x47
+#define MPU9250_GYRO_Z_LSB_REG 0x48
 
-#define ADXL345_DEVICE 0xe5
+// Read Magnetometer
+#define AK8963_MANGET_X_LSB_REG 0x03
+#define AK8963_MANGET_X_MSB_REG 0x04
+#define AK8963_MANGET_Y_LSB_REG 0x05
+#define AK8963_MANGET_Y_MSB_REG 0x06
+#define AK8963_MANGET_Z_LSB_REG 0x07
+#define AK8963_MANGET_Z_MSB_REG 0x08
 
-class adxl345
+// Configuration
+#define MPU9250_CONFIGURATION_REG 0x1A
+#define MPU9250_GYRO_CONFIG_REG 0x1B
+#define MPU9250_ACCEL_CONFIG2_REG 0x1D
+#define AK8963_CTRL1_REG 0x0A
+
+// Device name
+#define MPU9250_DEVICE_NAME 0x68
+#define AK8963_DEVICE_NAME 0x48 
+
+// Configuration
+#define MPU9250_EXT_SYNC_SET_DISABLE 0b00000000
+#define MPU9250_DLPF_CFG_0 0b000
+#define MPU9250_DLPF_CFG_1 0b001
+#define MPU9250_DLPF_CFG_2 0b010
+#define MPU9250_DLPF_CFG_3 0b011
+#define MPU9250_DLPF_CFG_4 0b100
+#define MPU9250_DLPF_CFG_5 0b101
+#define MPU9250_DLPF_CFG_6 0b110
+#define MPU9250_DLPF_CFG_7 0b111
+
+#define MPU9250_FCHOICE_B_BAND_0 0b00
+#define MPU9250_FCHOICE_B_BAND_1 0b01
+#define MPU9250_FCHOICE_B_BAND_3 0b11
+
+#define MPU9250_ACCELL_OUTPUT_BAND_1_13K 0b0000
+#define MPU9250_ACCELL_OUTPUT_BAND_460 0b1001
+#define MPU9250_ACCELL_OUTPUT_BAND_92 0b1010
+#define MPU9250_ACCELL_OUTPUT_BAND_41 0b1011
+#define MPU9250_ACCELL_OUTPUT_BAND_20 0b1100
+#define MPU9250_ACCELL_OUTPUT_BAND_10 0b1101
+#define MPU9250_ACCELL_OUTPUT_BAND_5 0b1110
+
+#define AK8963_BIT_16_OUTPUT 0b10000
+#define AK8963_BIT_14_OUTPUT 0b00000
+
+#define AK8963_MODE_POWER_DOWN 0b0000 
+#define AK8963_MODE_SINGLE 0b0001 
+#define AK8963_MODE_CONTINOUS1 0b0010
+#define AK8963_MODE_CONTINOUS2 0b0110 
+#define AK8963_MODE_EXTERNAL_TRIGGER 0b0100 
+#define AK8963_MODE_SELF_TEST 0b1000 
+#define AK8963_MODE_FUSE_ROM 0b1111
+
+class FaBo9Axis
 {
 public:
   bool searchDevice(void);
   void configuration(void);
   void powerOn(void);
-  void readXYZ(int *x, int *y, int *z);
-  byte readIntStatus();
-  void enableTap();
-  bool isDoubleTap(byte value);
-  bool isSingleTap(byte value);
+  void reset(void);
+  void readAccelXYZ(int *x, int *y, int *z);
+  void readGyroXYZ(int *x, int *y, int *z);
+  uint16_t readTemperature();
+  void readMagnetXYZ(int *x, int *y, int *z);
 private:
-  void readI2c(byte register_addr, int num, byte *buf);
-  void writeI2c(byte register_addr, byte value);
+  void readI2c(byte address, byte register_addr, int num, byte *buf);
+  void writeI2c(byte address, byte register_addr, byte value);
 };
 
-extern adxl345 fabo3Axis;
