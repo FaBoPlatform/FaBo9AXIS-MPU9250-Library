f7e75954010ae13b1597198efde5b92db02caeb5
Sasaki Akira <akira@Dev.local>
2016-02-11 17:10:15 +0900

    Update Magnetmeter.

--- examples/read9axis/read9axis.ino
+++ examples/read9axis/read9axis.ino
@@ -4,6 +4,7 @@
 FaBo9Axis fabo9axis;
 
 void setup() {
+  Serial.begin(9600);
   // デバイス初期化
   Serial.println("Checking I2C device...");
   if (fabo9axis.searchDevice()) {
@@ -25,6 +26,30 @@ void loop() {
   fabo9axis.readGyroXYZ(&gx, &gy, &gz);
   fabo9axis.readMagnetXYZ(&mx, &my, &mz);
   temp = fabo9axis.readTemperature();
+
+  Serial.print("ax: ");
+  Serial.print(ax);
+  Serial.print(" ay: ");
+  Serial.print(ay);
+  Serial.print(" az: ");
+  Serial.println(az);
+
+  Serial.print("gx: ");
+  Serial.print(gx);
+  Serial.print(" gy: ");
+  Serial.print(gy);
+  Serial.print(" gz: ");
+  Serial.println(az);
+
+  Serial.print("mx: ");
+  Serial.print(mx);
+  Serial.print(" my: ");
+  Serial.print(my);
+  Serial.print(" mz: ");
+  Serial.println(mz);
+
+  Serial.print("temp: ");
+  Serial.println(temp);
 
   delay(1000);
 }
